055fb8ded7102c0e4f169b4deba85d5558acb628
Opemipo Roberts <opemipo.roberts@yahoo.co.uk>
2025-01-06 20:13:57 +0100

    slavvo

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,24 @@ import pic from '../public/images/pexels-cottonbro-10677505.jpg';
 import girl from '../public/images/stage performance 3.png';
 import poster from '../public/images/xftghstd.png';
 import additionalImage1 from '../public/images/stage performance 2.png'; 
-import additionalImage2 from '../public/images/slavvo.jpg'; 
-import additionalImage3 from '../public/images/_DSC2826-Enhanced-NR+(1)-min.png';
+import additionalImage2 from '../public/images/stage performance.png'; 
+import additionalImage3 from '../public/images/stage performance 3.png';
+import slovvo from '../public/images/slavvo.jpg';
 
 
 const Home = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-[#f8f8f1] text-gray-900 px-2 sm:px-4 pt-[70px] md:pt-[80px]">
+    <div className="flex flex-col min-h-screen bg-[#f7f7f4] text-gray-900 px-2 sm:px-4 pt-[70px] md:pt-[80px]">
       
 
       {/* Title Section */}
       <div className="mb-4 sm:mb-6 md:mb-8 w-full">
-        <h1 className="text-[17vw] lg:text-[20vw] font-bold text-center mt-[-50px] lg:mt-[-100px] mb-[-40px] lg:mb-[-60px] font-audiowide ">SLAWO.</h1>
+        <h1 className="text-[15vw] lg:text-[18vw] font-bold text-center mt-[-50px] lg:mt-[-100px] mb-[-40px] lg:mb-[-60px] font-audiowide ">SLAVVO.</h1>
         <h2 className="text-[4vw] sm:text-[3vw] font-semibold text-right mr-2 sm:mr-12 mb-[-10px] font-audiowide">SOUL & REBELLION.</h2>
       </div>
 
       {/* First Image and Text Section */}
-      <div className="flex flex-col md:flex-row w-full lg:mb-8 ">
+      <div className="flex flex-col md:flex-row w-full lg:mb-8 mb-20 ">
         <div className="relative w-full md:w-2/4 h-[500px] md:h-[700px] flex items-end md:ml-9 mb-4 lg:mb-0">
           <Image 
             src={pic} 
@@ -48,7 +49,7 @@ const Home = () => {
       <div className="flex flex-col md:flex-row w-full mb-6 lg:mb-8 justify-between">
         <div className="flex flex-col justify-center w-full md:w-[50%] lg:pl-32 pr-4 lg:h-[500px] h-[500px] ">
           <h2 className="md:text-2xl text-lg md:mb-9 font-bold text-center font-audiowide md:mt-18">“SOUL & SOIR” : JAN 16 2025</h2>
-          <p style={{ letterSpacing: '0.19em', lineHeight: '1.9' }} className="md:text-base text-xl md:mb-2 mb-4 font-merriweather">
+          <p style={{ letterSpacing: '0.19em', lineHeight: '1.9' }} className="md:text-base text-base text-center md:text-left md:mb-2 mb-4 font-merriweather">
             join us for an evening of soul, rhythm, nostalgia, friendship, and emotion. the “Soul and Soir” live concert will be headlined by Midé Johnson and features iconic performances from Rania Yasmine, Emerald, Tanaparis, and Marina Santos. Portia, a comedienne and incredible host, will take the stage to light up the audience as she guides them through the live renditions. tickets are available for 5 CHF in the link below and 10 CHF at the concert entrance.
           </p>
           <a href="#" className="font-merriweather text-xs sm:text-sm underline hover:bg-black hover:text-white p-1 transition duration-300 flex items-center w-fit">
@@ -106,20 +107,20 @@ const Home = () => {
       <div className="flex flex-col md:flex-row w-full">
         {/* Left Column with Text */}
         <div className="md:w-1/2 flex flex-col justify-start p-8">
-          <h1 className="text-5xl font-bold mb-4 font-audiowide">SLAWO (EARLY ACCESS)</h1>
-          <h4 style={{ letterSpacing: '0.19em', lineHeight: '1.9' }} className="text-3xl font-semibold mb-4 font-merriweather">NOT JUST AN ALBUM. AN EXPERIENCE. FOR ALL IMMIGRANTS: LEGAL, ILLEGAL & LETHAL.</h4>
+          <h1 className="lg:text-xl font-semibold lg:mb-10 mb-4 font-merriweather text-base">SLAVVO (EARLY ACCESS)</h1>
+          <h4 style={{ letterSpacing: '0.19em', lineHeight: '1.9' }} className="lg:text-4xl text-xl font-bold mb-4 font-merriweather">NOT JUST AN ALBUM. AN EXPERIENCE. FOR ALL IMMIGRANTS: LEGAL, ILLEGAL & LETHAL.</h4>
           <a href="#" className="font-merriweather text-xs sm:text-sm underline hover:bg-black hover:text-white p-1 transition duration-300 flex items-center w-fit">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368" className="mr-1">
               <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/>
             </svg>
-            <span className='font-audiowide'> view exclusive tracklist & features</span>
+            <span className='font-audiowide'> view exclusive track list & features</span>
           </a>
         </div>
 
         {/* Right Column with Image */}
         <div className="md:w-1/2 relative h-[400px] sm:h-[500px] md:h-[700px]">
           <Image 
-            src={pic} 
+            src={slovvo} 
             alt="Your Description" 
             layout="fill" 
             className="object-cover" 
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,34 +2,34 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="flex flex-col bg-black text-white p-8 w-full ">
+    <div className="flex flex-col bg-gray-900 text-white p-8 w-full">
       {/* Flex container for Subscription and Social Links */}
-      <div className="flex w-full max-w-full justify-between mb-8">
+      <div className="flex w-full max-w-full justify-around mb-8">
         {/* Subscription Section */}
-        <div className="flex flex-col items-start ">
+        <div className="flex flex-col items-start">
           <h2 className="text-lg mb-4">SUBSCRIBE</h2>
           <p className="text-sm mb-4">Sign up with your email address to keep up with the plantation.</p>
           <form className="flex space-x-2 mb-2">
             <input
               type="email"
               placeholder="Email Address"
-              className="p-2 rounded bg-gray-800 border border-gray-600 placeholder-gray-400"
+              className="p-3 w-full border border-gray-400 bg-gray-800 placeholder-gray-400 rounded-md"
             />
-            <button className="p-2 bg-white text-black rounded">Sign Up</button>
+            <button className="p-3 bg-white text-black rounded-md hover:bg-black hover:text-white hover:border border-white">Sign Up</button>
           </form>
           <p className="text-sm">We respect your privacy.</p>
         </div>
 
         {/* Social Media Links Section */}
         <div className="flex flex-col items-end ml-auto">
-          <a href="#" className="text-sm underline">INSTAGRAM</a>
-          <a href="#" className="text-sm underline">YOUTUBE</a>
-          <a href="#" className="text-sm underline">TIKTOK</a>
+          <a href="#" className="text-sm underline mb-2">INSTAGRAM</a>
+          <a href="#" className="text-sm underline mb-2">YOUTUBE</a>
+          <a href="#" className="text-sm underline mb-2">TIKTOK</a>
         </div>
       </div>
 
       {/* Brand Name Section */}
-      <div className="flex justify-center mb-4">
+      <div className="flex justify-center mb-4 w-full">
         <h1 className="text-[10vw] font-bold">SLAWO</h1>
       </div>
     </div>
