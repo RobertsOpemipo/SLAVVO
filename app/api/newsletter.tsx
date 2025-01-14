f076611cea7a6140aae1edf33d135a0f4da9e155
Opemipo Roberts <opemipo.roberts@yahoo.co.uk>
2025-01-14 23:32:44 +0100

    slavvo

--- app/api/newsletter.tsx
+++ app/api/newsletter.tsx
@@ -1,4 +1,4 @@
-const mailchimp = require("@mailchimp/mailchimp_marketing")
+/*const mailchimp = require("@mailchimp/mailchimp_marketing")
 
 mailchimp.setConfig({
     apiKey: process.env.MAILCHIMP_API_KEY,
@@ -7,4 +7,4 @@ mailchimp.setConfig({
 
 export default function handler(req, res) {
     res.status(200).json({name:"John Doe"})
-}
+}*/
